6c75e890af5941ce42655d4179736a3b8735e7fa
Karan Garg <in.karan@outlook.com>
2026-06-17 21:22:41 +0530

    add gitignore file

---
+++ ConsoleAppForLinq/Program.cs
@@ -0,0 +1,9 @@
+﻿int [] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+var evenNumbers = from n in numbers
+                  where n % 2 == 0
+                  select n;
+
+                  foreach(var num in evenNumbers)
+                  {
+                    Console.WriteLine(num);
+                  }
